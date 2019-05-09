d5cb7693d628e847fc6bf37a7c04cbc335cc8d31
cossydmain <cossydmain12@gmail.com>
2019-05-09 09:12:09 +0300

    wamuyu

--- mango/urls.py
+++ mango/urls.py
@@ -5,7 +5,7 @@ app_name = 'mango'
 
 urlpatterns = [
     path('',views.MemberListview.as_view(), name='index'),
-    path('add_Member',views.MemberCreateView.as_view(),name='add_Member'),
+    path('add_Member', views.MemberCreateView.as_view(),name='add_Member'),
     path('<int:pk>/update', views.MemberUpdateView.as_view(),name ='update-member'),
     path('<int:pk>/Delete', views.MemberDeleteView.as_view(),name = 'Delete-member'),
 ]
